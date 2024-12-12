f1dc053a49891f0b8182f3d92b877b240b820768
leomartinez2020 <lowenhar@yahoo.com>
2024-12-12 12:00:00 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app" class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+    <QuestionDisplay :question="currentQuestion" />
+    <NextButton @click="getNextQuestion" />
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from 'vue'
+import QuestionDisplay from './components/QuestionDisplay.vue'
+import NextButton from './components/NextButton.vue'
+import questionsData from './questions.json'
+
+const questions = questionsData.questions
+
+const currentQuestionIndex = ref(0)
+
+const currentQuestion = computed(() => questions[currentQuestionIndex.value])
+
+const getNextQuestion = () => {
+  currentQuestionIndex.value = (currentQuestionIndex.value + 1) % questions.length
+}
+</script>
---
+++ src/components/QuestionDisplay.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white shadow-lg rounded-lg p-6 mb-4 w-full max-w-md text-center">
+    <p class="text-xl font-semibold">{{ question }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  question: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<style>
+.text-xl {
+  font-size: 2em;
+}
+</style>
