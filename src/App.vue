8b06ad406418c94552a1a49fff16f9f2ad7a55c3
leomartinez2020 <lowenhard@yahoo.com>
2025-01-28 12:58:48 -0500

    Add previous button

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app" class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
     <QuestionDisplay :question="currentQuestion" />
     <NextButton @click="getNextQuestion" />
+    <PreviousButton @click="getPreviousQuestion" />
   </div>
 </template>
 
@@ -9,6 +10,8 @@
 import { ref, computed } from 'vue'
 import QuestionDisplay from './components/QuestionDisplay.vue'
 import NextButton from './components/NextButton.vue'
+import PreviousButton from './components/PreviousButton.vue'
+
 //import questionsData from './questions.json'
 import questionsData from './translations.json'
 
@@ -21,4 +24,9 @@ const currentQuestion = computed(() => questions[currentQuestionIndex.value])
 const getNextQuestion = () => {
   currentQuestionIndex.value = (currentQuestionIndex.value + 1) % questions.length
 }
+
+const getPreviousQuestion = () => {
+  currentQuestionIndex.value = (currentQuestionIndex.value - 1) % questions.length
+}
+
 </script>
---
+++ src/components/PreviousButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <button 
+    class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition duration-300"
+  >
+    Previous Question
+  </button>
+</template>
+
+<script setup>
+// No additional logic needed in this component
+</script>
