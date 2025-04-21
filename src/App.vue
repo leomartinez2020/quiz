8a4212e054b46cd359d793cdb1af6f0b3fff11aa
leomartinez2020 <lowenhard@yahoo.com>
2025-04-21 11:13:37 -0500

    Add English phrasal verbs 2

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ import PreviousButton from './components/PreviousButton.vue'
 //import questionsData from './questions.json'
 //import questionsData from './translations.json'
 //import questionsData from './reportedspeech.json'
-import questionsData from './phrasals.json
+import questionsData from './phrasals.json'
 
 const questions = questionsData.questions
 
